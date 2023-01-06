797704570d1e2ecbeb7bf330733d101cbb1a3de2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-01-06 21:10:02 +0000

    Add files

---
+++ include/ishiko/pdf/linkoptions.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2023 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/pdf/blob/main/LICENSE.txt
+*/
+
+#ifndef GUARD_ISHIKO_CPP_PDF_LINKOPTIONS_HPP
+#define GUARD_ISHIKO_CPP_PDF_LINKOPTIONS_HPP
+
+#endif
