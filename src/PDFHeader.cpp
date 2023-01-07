34f3688c8a50c73c34a2586625be82abf5dea7b1
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2023-01-07 15:50:20 +0000

    Updates

--- src/PDFHeader.cpp
+++ src/PDFHeader.cpp
@@ -1,7 +1,14 @@
 /*
-    Copyright (c) 2023 Xavier Leclercq
+    Copyright (c) 2011-2023 Xavier Leclercq
     Released under the MIT License
     See https://github.com/ishiko-cpp/pdf/blob/main/LICENSE.txt
 */
 
 #include "PDFHeader.hpp"
+
+using namespace Ishiko;
+
+void PDFHeader::serialize(PDFOutputStream& output_stream) const
+{
+    output_stream.write("%PDF-1.7\n");
+}
--- src/PDFOutputStream.cpp
+++ src/PDFOutputStream.cpp
@@ -5,3 +5,27 @@
 */
 
 #include "PDFOutputStream.hpp"
+
+using namespace Ishiko;
+
+PDFOutputStream PDFOutputStream::Create(const boost::filesystem::path& path, Error& error)
+{
+    PDFOutputStream new_output_stream;
+    new_output_stream.create(path.c_str(), error);
+    return new_output_stream;
+}
+
+void PDFOutputStream::create(const boost::filesystem::path& path, Error& error)
+{
+    m_file.create(path.c_str(), error);
+}
+
+void PDFOutputStream::close()
+{
+    m_file.close();
+}
+
+void PDFOutputStream::write(const char* str)
+{
+    m_file.write(str);
+}
